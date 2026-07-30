c8412aa6eb3b383014c51816ac7af932211faf55
Nsw235 <273624837+Nsw235@users.noreply.github.com>
2026-07-30 14:11:41 +0200

    Add files via upload

--- app/layout.tsx
+++ app/layout.tsx
@@ -22,12 +22,7 @@ export default function RootLayout({ children }: { children: React.ReactNode }) 
   return (
     <html lang="fr">
       <head>
-        <link
-          rel="stylesheet"
-          href="https://unpkg.com/leaflet@1.9.4/dist/leaflet.css"
-          integrity="sha256-p4NxAoJBhIIN+hmNHrzRCf9tD/miZyoHS5obTRR9BMY="
-          crossOrigin=""
-        />
+        <link rel="stylesheet" href="https://api.mapbox.com/mapbox-gl-js/v3.7.0/mapbox-gl.css" />
       </head>
       <body>{children}</body>
     </html>
